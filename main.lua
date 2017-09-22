1e22e63376d8d8071e36067a8930ca477f4963b8
jordanfb <jmanfb@gmail.com>
2017-09-22 15:31:54 -0400

    added a main.lua

---
+++ main.lua
@@ -0,0 +1,21 @@
+
+
+function love.load(args)
+	--
+end
+
+function love.update(dt)
+	--
+end
+
+function love.draw()
+	--
+end
+
+function love.keypressed(key, unicode)
+	--
+end
+
+function love.mousepressed(x, y, button)
+	--
+end
