2e6016c90cf8644513cd8dc4aab3fd7f3b6511bb
jordanfb <jmanfb@gmail.com>
2017-09-23 03:31:19 -0400

    Finished the base game

--- main.lua
+++ main.lua
@@ -1,23 +1,96 @@
 require "player"
+require "enemy"
 
 player = Player()
+enemies = {}
+bullets = {}
+timeBetweenWaves = 5
+waveCountdown = timeBetweenWaves
+waveNumber = 0
+maxWaveNumber = 0
 
 function love.load(args)
-	--
+	love.window.setFullscreen(true)
+	love.graphics.setBackgroundColor(50, 50, 50)
 end
 
+
 function love.update(dt)
+	-- update the wave of zombies:
+	if #enemies == 0 then
+		waveCountdown = waveCountdown - dt
+	end
+	if waveCountdown <= 0 then
+		waveNumber = waveNumber + 1
+		maxWaveNumber = math.max(waveNumber, maxWaveNumber)
+		waveCountdown = timeBetweenWaves
+		for i = 1, math.pow(2, waveNumber-1) do
+			-- spawn enemies for the wave randomly in a circle
+			local f = math.random() * 2 * math.pi
+			local r = love.graphics.getWidth() + .5*love.graphics.getWidth()*math.random()
+			local x = math.cos(f) * r
+			local y = math.sin(f) * r
+			table.insert(enemies, Enemy(x, y, player))
+		end
+	end
 	player:update(dt)
+	for i = #enemies, 1, -1 do
+		enemies[i]:update(dt)
+		if enemies[i].health <= 0 then
+			table.remove(enemies, i) -- remove dead enemies
+		end
+	end
+	for i = #bullets, 1, -1 do
+		bullets[i]:update(dt)
+		for k, enemy in ipairs(enemies) do
+			bullets[i]:checkEnemyCollision(enemy) -- check to see if they hit an enemy
+		end
+		if not bullets[i].active then
+			table.remove(bullets, i) -- remove finished bullets
+		end
+	end
+
+	-- check if the player is dead, if so, reset everything:
+	if player.health <= 0 then
+		waveNumber = 0
+		waveCountdown = timeBetweenWaves
+		player.health = 100
+		enemies = {}
+		bullets = {}
+	end
 end
 
 function love.draw()
 	player:draw()
+	for k, v in ipairs(enemies) do
+		v:draw()
+	end
+	for k, v in ipairs(bullets) do
+		v:draw()
+	end
+	love.graphics.setColor(255, 255, 255)
+	love.graphics.printf("Wave: "..waveNumber, 20, 20, 1000)
+	love.graphics.printf("Best: "..maxWaveNumber, 220, 20, 1000)
+	love.graphics.printf("Enemies left: "..#enemies, 420, 20, 1000)
 end
 
 function love.keypressed(key, unicode)
-	--
+	if key == "escape" then
+		love.event.quit()
+	end
 end
 
-function love.mousepressed(x, y, button)
-	--
+function addBullet(b)
+	table.insert(bullets, b)
+end
+
+function rectangleCollisionCheck(x1, y1, w1, h1, x2, y2, w2, h2)
+	-- returns whether the two rectangles are touching
+	-- the x and y coordinates are the centers of the rectangles
+	if x1 + w1/2 > x2 - w2/2 and x1 - w1/2 < x2 + w2/2 then
+		if y1 + h1/2 > y2 - h2/2 and y1 - h1/2 < y2 + h2/2 then
+			return true -- they're colliding
+		end
+	end
+	return false
 end
--- player.lua
+++ player.lua
@@ -2,22 +2,30 @@
 A simple player class that is controlled via keyboard and mouse
 Some obvious problems:
 - you walk faster diagonally than straight
+- there's no way to determine what health you're at
+- there's no way to determine whether your weapons are reloaded
 ]]--
 
 Class = require "class"
+require "bullet"
 
 Player = Class{}
 
 
 function Player:init(health)
-	self.health = health or 100
-	self.speed = 100
+	self.health = 100--health or 100
+	self.speed = 200
 	self.x = 0
 	self.y = 0
 
 	self.color = {200, 255, 100, 255} -- if you don't have the fourth number it defaults to 255 alpha
-	self.width = 100
-	self.height = 200
+	self.width = 50
+	self.height = 100
+
+	self.swordCooldown = .25
+	self.gunCooldown = .1
+	self.swordTimer = 0
+	self.gunTimer = 0
 end
 
 function Player:update(dt)
@@ -26,10 +34,34 @@ function Player:update(dt)
 	-- for strings you could just type them in there like t = { hello = 5 }, but if you wanted it to start with a
 	-- control character you may have to do the following: t = { [.weird] = false }
 
+
+	-- handle movement:
 	local dx = boolVal[love.keyboard.isDown("d")] - boolVal[love.keyboard.isDown("a")]
 	local dy = boolVal[love.keyboard.isDown("s")] - boolVal[love.keyboard.isDown("w")]
 	self.x = self.x + dx * self.speed * dt -- make sure you use dt (delta time) to ensure you move the same
 	self.y = self.y + dy * self.speed * dt -- speed no matter the framerate
+
+
+	-- then check if the player is attacking and attack!
+	local f = math.atan2(love.mouse.getY() - self.y, love.mouse.getX() - self.x)
+	if love.mouse.isDown(1) then
+		-- the left mouse is down, fire bullets
+		if self.gunTimer <= 0 then
+			-- make a bullet
+			local b = Bullet(self.x, self.y, f)
+			addBullet(b) -- this is a function in the main file which just adds a bullet to the table
+			self.gunTimer = self.gunCooldown
+		end
+	elseif love.mouse.isDown(2) then
+		-- sword thing
+		self.swordTimer = self.swordCooldown
+	end
+	if self.gunTimer > 0 then
+		self.gunTimer = self.gunTimer - dt
+	end
+	if self.swordTimer > 0 then
+		self.swordTimer = self.swordTimer - dt
+	end
 end
 
 function Player:draw()
